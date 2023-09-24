b386d8348a90c9e2b93fb2144681f1d93c534788
y.gladkikh <y.gladkikh@tinkoff.ru>
2023-09-24 15:37:56 +0300

    Moving bird to an object

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,39 +1,27 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture bird;
+    Bird bird;
 
-    public static final float SCR_WIDTH = 1280;
-    public static final float SCR_HEIGHT = 720;
+    public static float SCR_WIDTH = 1280;
+    public static float SCR_HEIGHT = 720;
 
     OrthographicCamera camera; // пересчитывает размеры для различных экранов
-
-    int x, y;
-    int birdWith, birdHeight;
-    int speed = 5;
-    boolean up = true;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        bird = new Texture("bird.png");
-        camera = new OrthographicCamera();
+        bird = new Bird(0, 0, 5, 250, 200);
 
-        x = 0;
-        y = 0;
-
-        birdWith = 250;
-        birdHeight = 200;
-
-        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+        SCR_WIDTH = Gdx.graphics.getWidth();
+        SCR_HEIGHT = Gdx.graphics.getHeight();
     }
 
     @Override
@@ -41,20 +29,8 @@ public class MyGdxGame extends ApplicationAdapter {
         ScreenUtils.clear(1, 1, 1, 1);
         batch.begin();
 
-        batch.draw(bird, x, y, birdWith, birdHeight);
-
-        if (y >= SCR_HEIGHT) {
-            up = false;
-        }
-        if (y <= 0) {
-            up = true;
-        }
-
-        if (up) {
-            y += speed;
-        } else {
-            y -= speed;
-        }
+        bird.draw(batch);
+        bird.fly();
 
         batch.end();
     }
