082b52fdbce4d9835a2b85dd2fbc45e93d309576
y.gladkikh <y.gladkikh@tinkoff.ru>
2023-09-24 15:31:33 +0300

    Bird moving

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,52 +1,64 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture bird;
+    SpriteBatch batch;
+    Texture bird;
 
-	public static final float SCR_WIDTH = 1280;
-	public static final float SCR_HEIGHT = 720;
+    public static float SCR_WIDTH = 1280;
+    public static float SCR_HEIGHT = 720;
 
-	OrthographicCamera camera; // пересчитывает размеры для различных экранов
+    int x, y;
+    int birdWith, birdHeight;
+    int speed = 5;
+    boolean up = true;
 
-	int x, y;
-	int birdWith, birdHeight;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		bird = new Texture("bird.png");
-		camera = new OrthographicCamera();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        bird = new Texture("bird.png");
 
-		x = 0;
-		y = 0;
+        x = 0;
+        y = 0;
 
-		birdWith = 250;
-		birdHeight = 200;
+        birdWith = 250;
+        birdHeight = 200;
 
-		camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
-	}
+        SCR_WIDTH = Gdx.graphics.getWidth();
+        SCR_HEIGHT = Gdx.graphics.getHeight();
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 1, 1, 1);
+        batch.begin();
 
-		batch.draw(bird, x, y, birdWith, birdHeight);
+        batch.draw(bird, x, y, birdWith, birdHeight);
 
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		bird.dispose();
-	}
+        if (y >= SCR_HEIGHT) {
+            up = false;
+        }
+        if (y <= 0) {
+            up = true;
+        }
+
+        if (up) {
+            y += speed;
+        } else {
+            y -= speed;
+        }
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        bird.dispose();
+    }
 }
